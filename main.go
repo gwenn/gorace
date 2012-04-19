cda76a3cd66986c28b2ce2db18e5af43965c8420
gwenn <gtreguier@gmail.com>
2012-04-19 20:56:35 +0200

    Add page to edit race start time.

---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/gwenn/gosqlite"
+	"html/template"
+	"net/http"
+	"os"
+	"os/signal"
+	"path"
+	"strconv"
+)
+
+const (
+	DB_PATH        = "db/race.sqlite"
+	STATIC_PATH    = "static"
+	TMPL_PATH      = "tmpl"
+	LAPS_TMPL      = "laps.html"
+	TIME_LOGS_TMPL = "timelogs.html"
+	RESULTS_TMPL   = "results.html"
+	RACE_TMPL      = "race.html"
+)
+
+func errorHandler(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+func lapsHandler(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	teams, err := loadTeams(db)
+	if err != nil {
+		return
+	}
+	err = templates.ExecuteTemplate(w, LAPS_TMPL, teams)
+	return
+}
+
+func addTimeLogs(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	err = r.ParseForm()
+	if err != nil {
+		return
+	}
+	time := r.Form["time"]
+	if len(time) == 0 || len(time[0]) == 0 {
+		err = errors.New("Missing 'time' value")
+		return
+	}
+	teams := r.Form["teams"]
+	if len(teams) == 0 || len(teams[0]) == 0 {
+		err = errors.New("Missing 'teams' value(s)")
+		return
+	}
+	var teamIds []int = make([]int, len(teams))
+	for i, teamId := range teams {
+		teamIds[i], err = strconv.Atoi(teamId)
+		if err != nil {
+			warn("Invalid team id: %q (%s)\n", teamId, err)
+			return
+		}
+	}
+	err = saveTimeLogs(db, teamIds, time[0])
+	if err != nil {
+		return
+	}
+	timeLogs := make([]TimeLog, len(teamIds))
+	err = withTeamCache(db, func(cache *TeamCache) error {
+		for i, teamId := range teamIds {
+			timeLogs[i] = TimeLog{Team: cache.get(teamId), Time: time[0]}
+		}
+		return nil
+	})
+	b, err := json.Marshal(timeLogs)
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+	return
+}
+
+func displayTimeLogs(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	teamValue := r.FormValue("team") // optional
+	var timeLogs []TimeLog
+	if len(teamValue) > 0 {
+		var teamId int
+		teamId, err = strconv.Atoi(teamValue)
+		if err != nil {
+			warn("Invalid team id: %q (%s)\n", teamValue, err)
+			return
+		}
+		timeLogs, err = loadTimeLogsByTeam(db, teamId)
+	} else {
+		limitValue := r.FormValue("limit") // optional
+		limit := -1
+		if len(limitValue) > 0 {
+			limit, err = strconv.Atoi(limitValue)
+			if err != nil {
+				warn("Invalid limit: %q (%s)\n", limitValue, err)
+				return
+			}
+		}
+		timeLogs, err = loadTimeLogs(db, limit)
+	}
+	if err != nil {
+		return
+	}
+	err = templates.ExecuteTemplate(w, TIME_LOGS_TMPL, timeLogs)
+	return
+}
+
+func fixTimeLog(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	return // TODO update or delete
+}
+
+func displayResults(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	results, err := loadResults(db)
+	if err != nil {
+		return
+	}
+	err = templates.ExecuteTemplate(w, RESULTS_TMPL, results)
+	return
+}
+
+func displayRace(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	race, err := loadRace(db)
+	if err != nil {
+		return
+	}
+	err = templates.ExecuteTemplate(w, RACE_TMPL, race)
+	return
+}
+
+func setStartTime(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
+	err = r.ParseForm()
+	if err != nil {
+		return
+	}
+	time := r.FormValue("start_time")
+	if len(time) == 0 {
+		err = errors.New("Missing 'start_time' value")
+		return
+	}
+	err = saveRace(db, time)
+	if err != nil {
+		return
+	}
+	http.Redirect(w, r, "/laps", http.StatusFound)
+	return
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, *sqlite.Conn) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		db, err := getConn()
+		if err != nil {
+			errorHandler(w, err)
+			return
+		}
+		defer releaseConn(db)
+		err = fn(w, r, db)
+		if err != nil {
+			errorHandler(w, err)
+		}
+	}
+}
+
+var templates = template.Must(template.ParseFiles(
+	path.Join(TMPL_PATH, LAPS_TMPL),
+	path.Join(TMPL_PATH, TIME_LOGS_TMPL),
+	path.Join(TMPL_PATH, RESULTS_TMPL),
+	path.Join(TMPL_PATH, RACE_TMPL)))
+
+func main() {
+	fileServer := http.FileServer(http.Dir(STATIC_PATH))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/laps", makeHandler(lapsHandler))
+	http.HandleFunc("/timelogs/add", makeHandler(addTimeLogs))
+	http.HandleFunc("/timelogs/list", makeHandler(displayTimeLogs))
+	http.HandleFunc("/timelogs/fix", makeHandler(fixTimeLog))
+	http.HandleFunc("/results", makeHandler(displayResults))
+	http.HandleFunc("/race", makeHandler(displayRace))
+	http.HandleFunc("/race/start", makeHandler(setStartTime))
+
+	var interrupted = make(chan os.Signal)
+	go func() {
+		<-interrupted
+		closePool()
+		trace("Bye bye")
+		os.Exit(0)
+	}()
+	signal.Notify(interrupted, os.Interrupt)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fatal("error running race webserver: %v\n", err)
+	}
+}
--- race.go
+++ race.go
@@ -1,166 +1,52 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
+	"fmt"
 	"github.com/gwenn/gosqlite"
-	"html/template"
-	"net/http"
-	"os"
-	"os/signal"
-	"path"
-	"strconv"
 )
 
 const (
-	DB_PATH        = "db/race.sqlite"
-	STATIC_PATH    = "static"
-	TMPL_PATH      = "tmpl"
-	RACE_TMPL      = "race.html"
-	TIME_LOGS_TMPL = "timelogs.html"
-	RESULTS_TMPL   = "results.html"
+	RACE_QUERY  = `SELECT time FROM start_time`
+	RACE_UPDATE = `REPLACE INTO start_time VALUES (?)`
 )
 
-func errorHandler(w http.ResponseWriter, err error) {
-	http.Error(w, err.Error(), http.StatusInternalServerError)
+type Race struct {
+	StartTime string // "HH:MM:SS"
 }
 
-func raceHandler(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
-	teams, err := loadTeams(db)
+func loadRace(db *sqlite.Conn) (*Race, error) {
+	trace("Loading race...")
+	s, err := db.Prepare(RACE_QUERY)
 	if err != nil {
+		return nil, err
+	}
+	defer s.Finalize()
+	var r *Race = &Race{}
+	err = s.Select(func(s *sqlite.Stmt) (err error) {
+		err = s.Scan(&r.StartTime)
 		return
-	}
-	err = templates.ExecuteTemplate(w, RACE_TMPL, teams)
-	return
-}
-
-func addTimeLogs(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
-	err = r.ParseForm()
-	if err != nil {
-		return
-	}
-	time := r.Form["time"]
-	if len(time) == 0 || len(time[0]) == 0 {
-		err = errors.New("Missing 'time' value")
-		return
-	}
-	teams := r.Form["teams"]
-	if len(teams) == 0 || len(teams[0]) == 0 {
-		err = errors.New("Missing 'teams' value(s)")
-		return
-	}
-	var teamIds []int = make([]int, len(teams))
-	for i, teamId := range teams {
-		teamIds[i], err = strconv.Atoi(teamId)
-		if err != nil {
-			warn("Invalid team id: %q (%s)\n", teamId, err)
-			return
-		}
-	}
-	err = saveTimeLogs(db, teamIds, time[0])
-	if err != nil {
-		return
-	}
-	timeLogs := make([]TimeLog, len(teamIds))
-	err = withTeamCache(db, func(cache *TeamCache) error {
-		for i, teamId := range teamIds {
-			timeLogs[i] = TimeLog{Team: cache.get(teamId), Time: time[0]}
-		}
-		return nil
 	})
-	b, err := json.Marshal(timeLogs)
+	if err != nil {
+		return nil, err
+	}
+	trace("Loaded race.")
+	return r, nil
+}
+
+func saveRace(db *sqlite.Conn, time string) (err error) {
+	tracef("Saving race (%s)\n", time)
+	s, err := db.Prepare(RACE_UPDATE)
 	if err != nil {
 		return
 	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write(b)
-	return
-}
-
-func displayTimeLogs(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
-	teamValue := r.FormValue("team") // optional
-	var timeLogs []TimeLog
-	if len(teamValue) > 0 {
-		var teamId int
-		teamId, err = strconv.Atoi(teamValue)
-		if err != nil {
-			warn("Invalid team id: %q (%s)\n", teamValue, err)
-			return
-		}
-		timeLogs, err = loadTimeLogsByTeam(db, teamId)
-	} else {
-		limitValue := r.FormValue("limit") // optional
-		limit := -1
-		if len(limitValue) > 0 {
-			limit, err = strconv.Atoi(limitValue)
-			if err != nil {
-				warn("Invalid limit: %q (%s)\n", limitValue, err)
-				return
-			}
-		}
-		timeLogs, err = loadTimeLogs(db, limit)
-	}
+	defer s.Finalize()
+	n, err := s.ExecDml(time)
 	if err != nil {
 		return
-	}
-	err = templates.ExecuteTemplate(w, TIME_LOGS_TMPL, timeLogs)
-	return
-}
-
-func fixTimeLog(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
-	return // TODO update or delete
-}
-
-func displayResults(w http.ResponseWriter, r *http.Request, db *sqlite.Conn) (err error) {
-	results, err := loadResults(db)
-	if err != nil {
+	} else if n != 1 {
+		err = fmt.Errorf("No change while saving race (%s)", time)
 		return
 	}
-	err = templates.ExecuteTemplate(w, RESULTS_TMPL, results)
+	tracef("Race (%s) saved\n", time)
 	return
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, *sqlite.Conn) error) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		db, err := getConn()
-		if err != nil {
-			errorHandler(w, err)
-			return
-		}
-		defer releaseConn(db)
-		err = fn(w, r, db)
-		if err != nil {
-			errorHandler(w, err)
-		}
-	}
-}
-
-var templates = template.Must(template.ParseFiles(
-	path.Join(TMPL_PATH, RACE_TMPL),
-	path.Join(TMPL_PATH, TIME_LOGS_TMPL),
-	path.Join(TMPL_PATH, RESULTS_TMPL)))
-
-func main() {
-	fileServer := http.FileServer(http.Dir(STATIC_PATH))
-	http.Handle("/", fileServer)
-
-	http.HandleFunc("/race", makeHandler(raceHandler))
-	http.HandleFunc("/timelogs/add", makeHandler(addTimeLogs))
-	http.HandleFunc("/timelogs/list", makeHandler(displayTimeLogs))
-	http.HandleFunc("/timelogs/fix", makeHandler(fixTimeLog))
-	http.HandleFunc("/results", makeHandler(displayResults))
-
-	var interrupted = make(chan os.Signal)
-	go func() {
-		<-interrupted
-		closePool()
-		trace("Bye bye")
-		os.Exit(0)
-	}()
-	signal.Notify(interrupted, os.Interrupt)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fatal("error running race webserver: %v\n", err)
-	}
 }
